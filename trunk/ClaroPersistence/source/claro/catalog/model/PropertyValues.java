5356345cd92835be24b357e2596a06f285b2022f
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-11-17 21:31:07 +0000

    git-svn-id: file:///var/svn/claro-catalog@421 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- trunk/ClaroPersistence/source/claro/catalog/model/PropertyValues.java
+++ trunk/ClaroPersistence/source/claro/catalog/model/PropertyValues.java
@@ -1,12 +1,72 @@
 package claro.catalog.model;
 
-import java.util.ArrayList;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collection;
 
+import claro.jpa.catalog.Property;
 import claro.jpa.catalog.PropertyValue;
 
-public class PropertyValues extends ArrayList<PropertyValue> {
+public abstract class PropertyValues implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
+	public Object getValue() {
+		return getValue(null);
+	}
 	
+	public Object[] getValues() {
+		return getValues(null);
+	}
+	
+	public abstract Object[] getValues(String language);
+	public abstract Object getValue(String language);
+	abstract PropertyValues add(String language, PropertyValue value);
+}
+
+abstract class Single extends PropertyValues {
+  private static final long serialVersionUID = 1L;
+	public Object[] getValues(String language) {
+		return new Object[] { getValue(language) };
+	}
+}
+
+abstract class Many extends PropertyValues {
+  private static final long serialVersionUID = 1L;
+	public Object getValue(String language) {
+		Object[] values = getValues(language);
+		if (values.length < 1) {
+			return null;
+		}
+		return values[0];
+	}
+}
+
+class SingleEmpty extends Single {
+  private static final long serialVersionUID = 1L;
+	public Object getValue(String language) {
+		return null;
+  }
+  protected PropertyValues add(String language, PropertyValue value) {
+  	return language == null ? new SingleNullLanguageSingleValue(value) : new SingleSingleLanguageSingleValue(value); 
+  }
+}
+
+class SingleNullLanguageSingleValue extends Single {
+  private static final long serialVersionUID = 1L;
+	private final Object value;
+	public SingleNullLanguageSingleValue(Object value) {
+		this.value = value;
+  }
+	public Object getValue(String language) {
+		if (language == null) {
+			return value;
+		} else {
+			return null;
+		}
+	}
+}
+
+class SingleLanguageSingleValue extends Single {
 	String language;
 }
