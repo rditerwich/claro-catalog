48f217838de467095adf4838c222c6d1e4446c85
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-11-19 13:03:04 +0000

    git-svn-id: file:///var/svn/claro-catalog@429 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- trunk/ClaroPersistence/source/claro/catalog/PropertyData.java
+++ trunk/ClaroPersistence/source/claro/catalog/PropertyData.java
@@ -1,20 +1,18 @@
 package claro.catalog;
 
-import java.util.Map;
-
+import claro.catalog.util.SMap;
+import claro.jpa.catalog.Alternate;
 import claro.jpa.catalog.ImportSource;
 import claro.jpa.catalog.OutputChannel;
-import claro.jpa.catalog.Property;
 import claro.jpa.catalog.PropertyValue;
-import claro.jpa.catalog.Supplier;
 
 public class PropertyData {
 
-	Map<String, PropertyValue> values;
-	Map<String, PropertyValue> derivedValues;
-	Map<OutputChannel, Map<String, PropertyValue>> derivedOutputChannelValues;
-	Map<OutputChannel, Map<String, PropertyValue>> outputChannelSpecificValues;
-	Map<Supplier, Map<String, PropertyValue>> supplierValues;
-	Map<ImportSource, Map<String, PropertyValue>> importSourceValues;
+	SMap<String, PropertyValue> values;
+	SMap<String, PropertyValue> derivedValues;
+	SMap<OutputChannel, SMap<String, PropertyValue>> derivedOutputChannelValues;
+	SMap<OutputChannel, SMap<String, PropertyValue>> outputChannelSpecificValues;
+	SMap<Alternate, SMap<String, PropertyValue>> alternateValues;
+	SMap<ImportSource, SMap<String, PropertyValue>> importSourceValues;
 }
 	
