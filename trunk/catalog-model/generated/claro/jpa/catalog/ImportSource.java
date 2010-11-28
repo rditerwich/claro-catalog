02d91c2a750a2fc97c698f13a9494a7f10330a94
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-11-28 15:22:48 +0000

    git-svn-id: file:///var/svn/claro-catalog@464 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- trunk/catalog-model/generated/claro/jpa/catalog/ImportSource.java
+++ trunk/catalog-model/generated/claro/jpa/catalog/ImportSource.java
@@ -4,6 +4,9 @@ import java.lang.Integer;
 import java.lang.Long;
 import java.lang.Override;
 
+import javax.persistence.Entity;
+
+@Entity
 public class ImportSource {
     private Long id;
     private Integer priority;
