a1733ad6092b7022809862e53c56e0509ed1adf2
reinier.bos <reinier.bos@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-11-29 21:10:35 +0000

    git-svn-id: file:///var/svn/claro-catalog@477 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- trunk/catalog-model/source/claro/catalog/impl/ItemDetailsCommandImpl.java
+++ trunk/catalog-model/source/claro/catalog/impl/ItemDetailsCommandImpl.java
@@ -60,14 +60,14 @@ public class ItemDetailsCommandImpl extends ItemDetailsCommand implements Comman
 			
 			// fill propertyData
 			// TODO Ruud: is the clone necessary?
-			propertyData.values = SMap.create(channel, CommandService.clone(property.getValues(area, channel), valuesView));
-			propertyData.effectiveValues = SMap.create(area, SMap.create(channel, CommandService.clone(property.getEffectiveValues(area, channel), effectiveValuesView)));
+			propertyData.values = SMap.create(channel, property.getValues(area, channel));
+			propertyData.effectiveValues = SMap.create(area, SMap.create(channel, property.getEffectiveValues(area, channel)));
 			
 			// TODO How to do this???
-//			propertyData.importSourceValues = SMap.create(channel, CommandService.clone(property.getImportSourceValues(null), importValuesView));
+//			propertyData.importSourceValues = SMap.create(channel, property.getImportSourceValues(null)));
 
 			// Add propertyinfo to result
-			result.propertyData.add(CommandService.clone(property.getPropertyInfo(), propertyInfoView), propertyData);
+			result.propertyData.add(property.getPropertyInfo(), propertyData);
 		}
 		
 		return result;
