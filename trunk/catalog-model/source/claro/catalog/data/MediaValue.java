7bab5c0f4fbef18246ccb80ac2c78aa5f6571a6b
reinier.bos <reinier.bos@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2011-01-21 09:13:01 +0000

    git-svn-id: file:///var/svn/claro-catalog@579 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- trunk/catalog-model/source/claro/catalog/data/MediaValue.java
+++ trunk/catalog-model/source/claro/catalog/data/MediaValue.java
@@ -6,10 +6,13 @@ public class MediaValue implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	public final Long propertyValueId;
-	public final String mimeType;
-	public final String filename;
-
+	public Long propertyValueId;
+	public String mimeType;
+	public String filename;
+	
+	public MediaValue() {
+	}
+	
 	public MediaValue(Long propertyValueId, String mimeType, String filename) {
 		this.propertyValueId = propertyValueId;
 		this.mimeType = mimeType;
