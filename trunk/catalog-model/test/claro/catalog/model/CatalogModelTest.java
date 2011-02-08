9dcd8f430fea5cdb985dba0cdb62d901bf096850
reinier.bos <reinier.bos@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2011-02-08 22:22:11 +0000

    git-svn-id: file:///var/svn/claro-catalog@650 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- trunk/catalog-model/test/claro/catalog/model/CatalogModelTest.java
+++ trunk/catalog-model/test/claro/catalog/model/CatalogModelTest.java
@@ -1,14 +1,22 @@
 package claro.catalog.model;
 
+import static claro.catalog.model.CatalogModelTestUtil.addCategory;
+
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.persistence.EntityManager;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import claro.catalog.model.test.util.CatalogTestBase;
+import claro.jpa.catalog.Category;
+import easyenterprise.lib.util.SMap;
 
 public class CatalogModelTest extends CatalogTestBase {
-
 	@Test
 	public void test() throws IOException, SQLException {
 //		DBScript script = new DBScript("DROP SCHEMA catalog CASCADE");
@@ -23,4 +31,26 @@ public class CatalogModelTest extends CatalogTestBase {
 	
 	
 	// TODO Add test to invalidate item without childextent.
+	
+	
+	@Test
+	public void testEffectiveValues() throws Exception {
+		ensureDatabaseCreated();
+
+		EntityManager entityManager = getCatalogDao().getEntityManager();
+		CatalogModel model = getCatalogModel();
+
+		Category root = model.catalog.getRoot();
+
+		Category printers = addCategory(entityManager, model, root, "Printers");
+		
+		ItemModel printerModel = model.getItem(printers.getId());
+
+		PropertyModel visibleProperty = printerModel.findProperty(model.visibleProperty.getEntity(), true);
+		Assert.assertNotNull(visibleProperty);
+		
+		SMap<String,Object> effectiveValues = visibleProperty.getEffectiveValues(null, null);
+		Object defaultLanguageValue = effectiveValues.get(null);
+		Assert.assertTrue((Boolean) defaultLanguageValue);
+	}
 }
